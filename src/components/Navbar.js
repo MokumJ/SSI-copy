ce6383a4fd0d7478be10f6f4eb40d2cb2b9dc2d0
JasonNelemans <jasonnelemans@hotmail.com>
2020-06-18 16:57:59 +0200

    styled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react"
+import styled from "styled-components"
+import Link from "gatsby-link"
+import logo from "../images/SSI-logo.jpg"
+import "../styles/navbar.css"
 
 const StyledNavbar = styled.div`
-border-bottom: 1px solid;
-width: auto;
-text-align: center;
-padding-bottom: 50px;
+  border-bottom: 1px solid;
+  width: auto;
+  text-align: center;
+`
+
+const MainNav = styled.div`
+  margin-left: auto;
+  height: 100%;
+  display: block;
+  padding-left: 15px;
+  vertical-align: top;
+  text-align: center;
+  display: flex;
 `
 
 export default function Navbar() {
   return (
-    <StyledNavbar>this is the navbar</StyledNavbar>
+    <StyledNavbar>
+      <div className="nav-container">
+        <img src={logo} alt="logo" widht="215" height="88" />
+        <nav className="menu">
+          <ul>
+            <li>
+              <Link to="/" activeStyle={{color: '#0ea47c'}}>HOME</Link>
+            </li>
+            <li>
+              <Link to="#" activeStyle={{color: '#0ea47c'}}>ABOUT</Link>
+            </li>
+            <li>
+              <Link to="#" activeStyle={{color: '#0ea47c'}}>CONTACT</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </StyledNavbar>
   )
 }
