c9cafe0967c2b0d96f9cba8b78c27c201a6f796c
JasonNelemans <jasonnelemans@hotmail.com>
2020-06-23 14:26:35 +0200

    finished front page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,15 @@ import styled from 'styled-components';
 
 const StyledFooter = styled.div`
 text-align: center;
-padding-bottom: 100px;
+padding-bottom: 70px;
+padding-top: 35px;
 border-top: 1px solid;
 background-color: #3a3a3a;
+color: #fff;
 `
 
 export default function Footer() {
   return (
-    <StyledFooter>This is the footer</StyledFooter>
+    <StyledFooter>© 2020 Smeets Social Impact | Disclaimer</StyledFooter>
   )
 }
