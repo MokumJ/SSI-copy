8ef6c6dce8aeee6b739b8cbdf08100f6544f99a3
JasonNelemans <jasonnelemans@hotmail.com>
2020-06-25 23:57:21 +0200

    added placeholder image

--- src/components/Quotes/Quotes.jsx
+++ src/components/Quotes/Quotes.jsx
@@ -1,25 +1,30 @@
 import React, { useState } from "react"
-import { css } from "emotion"
+import { css } from "emotion";
+import placeholder from "../../images/placeholder-person.jpg"
 
 const Quote = () => {
   const quotes = {
     0: {
       client: "Awesome client, Acme Co.",
+      img: placeholder,
       quote:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget erat auctor, faucibus ante ac, convallis metus. Nunc facilisis varius semper. Nullam ut augue eu ipsum rhoncus suscipit in ac diam. Vivamus mattis est justo, in congue nunc rutrum eu. Nullam non elit ultrices, mollis ipsum ut, lobortis quam. Vestibulum auctor erat eu ligula tincidunt, a molestie erat mattis. Nam congue placerat dui, at congue nisl eleifend id. Cras quis odio ornare, tristique diam eu, egestas nisi. Ut pulvinar mattis quam quis blandit. Nunc accumsan efficitur enim, non elementum turpis consectetur a. Mauris nec mi non lacus mattis feugiat ac eget nisi. Aenean accumsan diam vitae auctor consectetur. Ut et enim scelerisque, volutpat erat eget, ornare diam. Sed vel ultrices tortor. Sed mattis sed felis vitae tristique. Aliquam venenatis nulla ante, ut finibus nibh dignissim at.",
     },
     1: {
       client: "Sweet Client, Acme Inc.",
+      img: placeholder,
       quote:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget erat auctor, faucibus ante ac, convallis metus. Nunc facilisis varius semper. Nullam ut augue eu ipsum rhoncus suscipit in ac diam. Vivamus mattis est justo, in congue nunc rutrum eu. Nullam non elit ultrices, mollis ipsum ut, lobortis quam. Vestibulum auctor erat eu ligula tincidunt, a molestie erat mattis. Nam congue placerat dui, at congue nisl eleifend id. Cras quis odio ornare, tristique diam eu, egestas nisi. Ut pulvinar mattis quam quis blandit. Nunc accumsan efficitur enim, non elementum turpis consectetur a. Mauris nec mi non lacus mattis feugiat ac eget nisi. Aenean accumsan diam vitae auctor consectetur. Ut et enim scelerisque, volutpat erat eget, ornare diam. Sed vel ultrices tortor. Sed mattis sed felis vitae tristique. Aliquam venenatis nulla ante, ut finibus nibh dignissim at.",
     },
     2: {
       client: "Dope Client, Acme LLC",
+      img: placeholder,
       quote:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget erat auctor, faucibus ante ac, convallis metus. Nunc facilisis varius semper. Nullam ut augue eu ipsum rhoncus suscipit in ac diam. Vivamus mattis est justo, in congue nunc rutrum eu. Nullam non elit ultrices, mollis ipsum ut, lobortis quam. Vestibulum auctor erat eu ligula tincidunt, a molestie erat mattis. Nam congue placerat dui, at congue nisl eleifend id. Cras quis odio ornare, tristique diam eu, egestas nisi. Ut pulvinar mattis quam quis blandit. Nunc accumsan efficitur enim, non elementum turpis consectetur a. Mauris nec mi non lacus mattis feugiat ac eget nisi. Aenean accumsan diam vitae auctor consectetur. Ut et enim scelerisque, volutpat erat eget, ornare diam. Sed vel ultrices tortor. Sed mattis sed felis vitae tristique. Aliquam venenatis nulla ante, ut finibus nibh dignissim at.",
     },
     3: {
       client: "General Client, Acme BV",
+      img: placeholder,
       quote:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget erat auctor, faucibus ante ac, convallis metus. Nunc facilisis varius semper. Nullam ut augue eu ipsum rhoncus suscipit in ac diam. Vivamus mattis est justo, in congue nunc rutrum eu. Nullam non elit ultrices, mollis ipsum ut, lobortis quam. Vestibulum auctor erat eu ligula tincidunt, a molestie erat mattis. Nam congue placerat dui, at congue nisl eleifend id. Cras quis odio ornare, tristique diam eu, egestas nisi. Ut pulvinar mattis quam quis blandit. Nunc accumsan efficitur enim, non elementum turpis consectetur a. Mauris nec mi non lacus mattis feugiat ac eget nisi. Aenean accumsan diam vitae auctor consectetur. Ut et enim scelerisque, volutpat erat eget, ornare diam. Sed vel ultrices tortor. Sed mattis sed felis vitae tristique. Aliquam venenatis nulla ante, ut finibus nibh dignissim at.",
     },
@@ -41,10 +46,10 @@ const Quote = () => {
         flex-direction: column;
         align-items: center;
         margin: 40px auto;
-        max-width: 700px;
+        max-width: 1000px;
         padding: 40px;
         border: 1px solid lightgrey;
-        box-shadow: 0 0 3px #ccc;
+        box-shadow: 0 0 3px #ccc; 
 
         p {
           text-align: center;
@@ -54,7 +59,8 @@ const Quote = () => {
           font-size: 15px;
         }
       `}
-    >
+    > {console.log(current.img)}
+      <img src={placeholder} alt="portrait" height="150px" width="150px"  />
       <p>{current.client}</p>
       <p>{current.quote}</p>
       <div
@@ -81,11 +87,11 @@ const Quote = () => {
           }
 
           span:hover::before {
-            background-color: #45454d;
+            background-color: #31A069;
           }
 
           span[data-quote="${active}"]::before {
-            background-color: #45454d;
+            background-color: #31A069;
           }
 
         `}
